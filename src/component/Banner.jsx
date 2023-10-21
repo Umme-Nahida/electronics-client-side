2600101f068d648e1146a8fc9893bac5585db40c
emamuddin10 <imamu854@gmail.com>
2023-10-21 22:49:27 +0600

    create a banner setion and create api and fake data for 6 type of brand

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     return (
         <div className="bg-[url('https://i.ibb.co/kM37NPy/beautiful-photograph-wallpaper-background-853645-10508.jpg')] bg-no-repeat bg-cover bg-opacity-70 bg-amber-400 ">
              {/* navbar */}
-          <div className="flex items-center justify-center py-10 px-5 md:py-12 md:px-10 lg:py-24 lg:px-28  bg-blue-700  bg-opacity-40">
+             <div className="flex items-center justify-center py-10 px-5 md:py-12 md:px-10 lg:py-24 lg:px-28  bg-blue-700  bg-opacity-40">
             <div className=" navbar-center  text-center" data-aos ="fade-up" data-aos-duration="2000">
                 <h1 className=" text-2xl md:text-4xl lg:text-7xl  mb-10 text-[#fffffffb] font-semibold shadow-md">Online store of household <br /> <span className="text-5xl">appliances and electronics</span></h1>
                 <p className="text-lg font-semibold text-[#fffffffb]">Now the result is just needed for the border gate. Massage therapy now, makeup anyone but now.</p>

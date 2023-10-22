dc8d9666d4e9b01337b9251598e78753c8bd69c7
emamuddin10 <imamu854@gmail.com>
2023-10-22 13:22:55 +0600

    connect to the firebase on the cliient side

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className=" navbar-center  text-center" data-aos ="fade-up" data-aos-duration="2000">
                 <h1 className=" text-2xl md:text-4xl lg:text-7xl  mb-10 text-[#ecc517fb] font-semibold shadow-md">Online store of household <br /> <span className="text-xl md:text-2xl lg:text-4xl">appliances and electronics</span></h1>
                 <p className="text-sm md:text-lg lg:text-lg w-96 md:w-[700px] lg:w-[800px] mx-auto font-bold text-[#fffffffb]">Our company serves as an innovation hub where some of the brightest minds in the industry collaborate to create breakthrough products. We invest heavily in research and development to stay ahead in the fast-paced electronics sector. Our products not only keep up with the latest trends but often set the trends themselves.</p>
-                <button className="btn btn-warning rounded-full py-4 px-8 mt-10  text-[#FFFFFF]">GET STARTED</button>
+                <button className="btn btn-warning rounded-full py-4 px-8 mt-10  text-[#FFFFFF]">Get Started</button>
             </div>
         </div>
         </div>
